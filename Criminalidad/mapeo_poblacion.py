4e0df694347f1e4c20b53ba7b761426b9c014b78
Gustavo Vasquez <gustavovasquez99@icloud.com>
2023-05-24 13:37:07 -0600

    Data Prep Terminado

--- Criminalidad/mapeo_poblacion.py
+++ Criminalidad/mapeo_poblacion.py
@@ -21,3 +21,17 @@ def get_population(value):
     if value in poblacion_alcaldias:
         return poblacion_alcaldias[value]
     return 0
+
+def obtener_rango_edad(edad):
+    if edad <= 12:
+        return "Niños"
+    elif edad <= 19:
+        return "Adolescentes"
+    elif edad <= 39:
+        return "Adultos jóvenes"
+    elif edad <= 59:
+        return "Adultos de mediana edad"
+    elif edad <= 79:
+        return "Adultos mayores"
+    else:
+        return "Ancianos/as"
